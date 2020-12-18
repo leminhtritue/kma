d595d35cecf9a6cbe16f4ca6b2fe8cf133488afd
leminhtritue <leminhtritue@gmail.com>
2020-12-18 21:33:58 +0700

    Update uda_digit.py

--- shot/uda_digit.py
+++ shot/uda_digit.py
@@ -282,7 +282,7 @@ def train_target(args):
     for k, v in netB.named_parameters():
         param_group += [{'params': v, 'lr': args.lr}]
     for k, v in netC.named_parameters():
-        param_group += [{'params': v, 'lr': learning_rate}]    
+        param_group += [{'params': v, 'lr': args.lr}]    
 
     optimizer = optim.SGD(param_group)
     optimizer = op_copy(optimizer)
