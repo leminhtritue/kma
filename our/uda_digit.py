e1a40de9aaeaf03872e86474c0ed0e5748d9ccce
leminhtritue <leminhtritue@gmail.com>
2020-12-18 22:56:02 +0700

    Update uda_digit.py

--- our/uda_digit.py
+++ our/uda_digit.py
@@ -348,15 +348,15 @@ def train_target(args):
         if inputs_test.size(0) == 1:
             continue
 
-        if iter_num % interval_iter == 0 and args.cls_par > 0:
-            netF.eval()
-            netB.eval()
-            netC.eval()
-            mem_label = obtain_label(dset_loaders['target_te'], netF, netB, netC, args)
-            mem_label = torch.from_numpy(mem_label).cuda()
-            netF.train()
-            netB.train()
-            netC.train()
+        # if iter_num % interval_iter == 0 and args.cls_par > 0:
+        #     netF.eval()
+        #     netB.eval()
+        #     netC.eval()
+        #     mem_label = obtain_label(dset_loaders['target_te'], netF, netB, netC, args)
+        #     mem_label = torch.from_numpy(mem_label).cuda()
+        #     netF.train()
+        #     netB.train()
+        #     netC.train()
 
         iter_num += 1
         lr_scheduler(optimizer, iter_num=iter_num, max_iter=max_iter)
