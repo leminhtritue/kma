8bc3bd325be4efa7e62245bb8e80ce257d3575f0
leminhtritue <leminhtritue@gmail.com>
2020-12-18 22:52:30 +0700

    Update uda_digit.py

--- our/uda_digit.py
+++ our/uda_digit.py
@@ -200,9 +200,9 @@ def train_source(args):
     interval_iter = max_iter // 10
     iter_num = 0
 
-    # netF.train()
-    # netB.train()
-    # netC.train()
+    netF.eval()
+    netB.eval()
+    netC.eval()
     total_loss = 0.0
     count_loss = 0
 
