08a1b5d3753c536400e6649d0d32a5dc3a10f799
leminhtritue <leminhtritue@gmail.com>
2020-12-19 00:04:54 +0700

    Update network.py

--- our/network.py
+++ our/network.py
@@ -34,7 +34,7 @@ class feat_bootleneck(nn.Module):
         self.feature_map.new_feature_map()
 
     def forward(self, x):
-        x = self.feature_map(x)
+        # x = self.feature_map(x)
         # x = self.bottleneck(x)
         # if self.type == "bn":
         #     x = self.bn(x)
@@ -45,7 +45,7 @@ class feat_classifier(nn.Module):
     def __init__(self, class_num, bottleneck_dim=256, type="linear"):
         super(feat_classifier, self).__init__()
         if type == "linear":
-            self.fc = nn.Linear(32, class_num)
+            self.fc = nn.Linear(256, class_num)
         else:
             self.fc = weightNorm(nn.Linear(bottleneck_dim, class_num), name="weight")
         self.fc.apply(init_weights)
