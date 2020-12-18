41326dfeaf4122011570af1545bf45aae492f86b
leminhtritue <leminhtritue@gmail.com>
2020-12-19 00:05:38 +0700

    Update network.py

--- our/network.py
+++ our/network.py
@@ -45,7 +45,7 @@ class feat_classifier(nn.Module):
     def __init__(self, class_num, bottleneck_dim=256, type="linear"):
         super(feat_classifier, self).__init__()
         if type == "linear":
-            self.fc = nn.Linear(256, class_num)
+            self.fc = nn.Linear(800, class_num)
         else:
             self.fc = weightNorm(nn.Linear(bottleneck_dim, class_num), name="weight")
         self.fc.apply(init_weights)
