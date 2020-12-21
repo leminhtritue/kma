749814275b57c2d08541d6c077ed150a9b9ec4f0
leminhtritue <leminhtritue@gmail.com>
2020-12-21 16:50:28 +0700

    Update network.py

--- our/network.py
+++ our/network.py
@@ -34,7 +34,7 @@ class feat_bootleneck(nn.Module):
         # self.feature_map = RandomFourierFeatures(feature_dim, 32)
         # self.feature_map.new_feature_map()
 
-        self.rf_dim = 512
+        self.rf_dim = 32
         self.omega = torch.zeros(feature_dim, self.rf_dim//2).cuda()
         self.omega.normal_()
         self.softmax_temp = 1/math.sqrt(feature_dim)
@@ -56,7 +56,7 @@ class feat_classifier(nn.Module):
     def __init__(self, class_num, bottleneck_dim=256, type="linear"):
         super(feat_classifier, self).__init__()
         if type == "linear":
-            self.fc = nn.Linear(512, class_num)
+            self.fc = nn.Linear(32, class_num)
         else:
             self.fc = weightNorm(nn.Linear(bottleneck_dim, class_num), name="weight")
         self.fc.apply(init_weights)
