0f786f869606207efb55b73a8229feccc3e5d3b7
leminhtritue <leminhtritue@gmail.com>
2020-12-21 23:36:28 +0700

    Update uda_digit.py

--- our/uda_digit.py
+++ our/uda_digit.py
@@ -321,11 +321,11 @@ def test_dataset(args):
     # netC = network.feat_classifier(type=args.layer, class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
     netC = network.feat_classifier(type="linear", class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
 
-    args.modelpath = args.output_dir + '/target_F_par_0.0.pt'   
+    args.modelpath = args.output_dir + '/source_F.pt'   
     netF.load_state_dict(torch.load(args.modelpath))
-    args.modelpath = args.output_dir + '/target_B_par_0.0.pt'   
+    args.modelpath = args.output_dir + '/source_B.pt'   
     netB.load_state_dict(torch.load(args.modelpath))
-    args.modelpath = args.output_dir + '/target_C_par_0.0.pt'   
+    args.modelpath = args.output_dir + '/source_C.pt'   
     netC.load_state_dict(torch.load(args.modelpath))
     netF.eval()
     netB.eval()
@@ -351,11 +351,11 @@ def extract_hyperplane(args):
     # netC = network.feat_classifier(type=args.layer, class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
     netC = network.feat_classifier(type="linear", class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
 
-    args.modelpath = args.output_dir + '/target_F_par_0.0.pt'   
+    args.modelpath = args.output_dir + '/source_F.pt'   
     netF.load_state_dict(torch.load(args.modelpath))
-    args.modelpath = args.output_dir + '/target_B_par_0.0.pt'   
+    args.modelpath = args.output_dir + '/source_B.pt'   
     netB.load_state_dict(torch.load(args.modelpath))
-    args.modelpath = args.output_dir + '/target_C_par_0.0.pt'   
+    args.modelpath = args.output_dir + '/source_C.pt'   
     netC.load_state_dict(torch.load(args.modelpath))
     netF.eval()
     netB.eval()
