6c538a050a70647b6b8fe3f20f333860eff89f1f
leminhtritue <leminhtritue@gmail.com>
2020-12-31 04:20:24 +0700

    Update network.py

--- our_maxmax_4096/network.py
+++ our_maxmax_4096/network.py
@@ -135,22 +135,19 @@ class DTNBase(nn.Module):
         super(DTNBase, self).__init__()
         self.conv_params = nn.Sequential(
                 nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=2),
-                # nn.BatchNorm2d(64),
-                # nn.Dropout2d(0.1),
-                nn.MaxPool2d(2),
+                nn.BatchNorm2d(64),
+                nn.Dropout2d(0.1),
                 nn.ReLU(),
                 nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
-                # nn.BatchNorm2d(128),
-                # nn.Dropout2d(0.3),
-                nn.Dropout2d(0.5),
-                nn.MaxPool2d(2),
+                nn.BatchNorm2d(128),
+                nn.Dropout2d(0.3),
                 nn.ReLU(),
-                # nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2),
-                # nn.BatchNorm2d(256),
-                # nn.Dropout2d(0.5),
-                # nn.ReLU()
+                nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2),
+                nn.BatchNorm2d(256),
+                nn.Dropout2d(0.5),
+                nn.ReLU()
                 )   
-        self.in_features = 128*4*4
+        self.in_features = 256*4*4
 
     def forward(self, x):
         x = self.conv_params(x)
