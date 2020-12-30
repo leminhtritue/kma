bf0e10ca2d852f226079f9b5da91ed11b5e2b695
leminhtritue <leminhtritue@gmail.com>
2020-12-31 04:15:30 +0700

    Update network.py

--- our_maxmax_4096/network.py
+++ our_maxmax_4096/network.py
@@ -137,17 +137,14 @@ class DTNBase(nn.Module):
                 nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=2),
                 # nn.BatchNorm2d(64),
                 # nn.Dropout2d(0.1),
-                nn.MaxPool2d(2),
                 nn.ReLU(),
                 nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
                 # nn.BatchNorm2d(128),
                 # nn.Dropout2d(0.3),
-                nn.MaxPool2d(2),
                 nn.ReLU(),
                 nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2),
                 # nn.BatchNorm2d(256),
                 nn.Dropout2d(0.5),
-                # nn.MaxPool2d(2),
                 nn.ReLU()
                 )   
         self.in_features = 256*4*4
