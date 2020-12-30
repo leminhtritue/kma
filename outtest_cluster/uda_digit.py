2cbc890d19cfae9f85b53c5be8f4619ea3c1cc2f
leminhtritue <leminhtritue@gmail.com>
2020-12-30 15:50:05 +0700

    Update uda_digit.py

--- outtest_cluster/uda_digit.py
+++ outtest_cluster/uda_digit.py
@@ -657,26 +657,33 @@ def obtain_label(loader, netF, netB, netC, args, c=None):
     all_output = nn.Softmax(dim=1)(all_output)
     _, predict = torch.max(all_output, 1)
     accuracy = torch.sum(torch.squeeze(predict).float() == all_label).item() / float(all_label.size()[0])
+
     
-    all_fea = torch.cat((all_fea, torch.ones(all_fea.size(0), 1)), 1)
-    all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
-    all_fea = all_fea.float().cpu().numpy()
+    # all_fea = torch.cat((all_fea, torch.ones(all_fea.size(0), 1)), 1)
+    # all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
+    # all_fea = all_fea.float().cpu().numpy() #60000x4097
 
-    K = all_output.size(1)
-    aff = all_output.float().cpu().numpy()
-    initc = aff.transpose().dot(all_fea)
-    initc = initc / (1e-8 + aff.sum(axis=0)[:,None])
-    dd = cdist(all_fea, initc, 'cosine')
-    pred_label = dd.argmin(axis=1)
-    acc = np.sum(pred_label == all_label.float().numpy()) / len(all_fea)
+    print("a")
+    distance = cdist(all_fea, all_fea, p=2)
+    print(distance.shape)
+    sys.exit()
 
-    for round in range(1):
-        aff = np.eye(K)[pred_label]
-        initc = aff.transpose().dot(all_fea)
-        initc = initc / (1e-8 + aff.sum(axis=0)[:,None])
-        dd = cdist(all_fea, initc, 'cosine')
-        pred_label = dd.argmin(axis=1)
-        acc = np.sum(pred_label == all_label.float().numpy()) / len(all_fea)
+    # K = all_output.size(1)
+    # aff = all_output.float().cpu().numpy()
+    # initc = aff.transpose().dot(all_fea)
+    # initc = initc / (1e-8 + aff.sum(axis=0)[:,None])
+    # dd = cdist(all_fea, initc, 'cosine')
+    # pred_label = dd.argmin(axis=1)
+    # acc = np.sum(pred_label == all_label.float().numpy()) / len(all_fea)
+
+    # for round in range(1):
+    #     aff = np.eye(K)[pred_label]
+    #     print(aff.shape)
+    #     initc = aff.transpose().dot(all_fea)
+    #     initc = initc / (1e-8 + aff.sum(axis=0)[:,None])
+    #     dd = cdist(all_fea, initc, 'cosine')
+    #     pred_label = dd.argmin(axis=1)
+    #     acc = np.sum(pred_label == all_label.float().numpy()) / len(all_fea)
 
     log_str = 'Accuracy = {:.2f}% -> {:.2f}%'.format(accuracy*100, acc*100)
     args.out_file.write(log_str + '\n')
@@ -744,7 +751,7 @@ if __name__ == "__main__":
     args.out_file.write(print_args(args)+'\n')
     args.out_file.flush()
 
-    test_target(args)
+    # test_target(args)
     train_target(args)
 
     cur_acc = test_dataset(args)
