f7fda24c94f276e791ff9c4ea3a0dcad8432f587
leminhtritue <leminhtritue@gmail.com>
2020-12-30 17:02:11 +0700

    Update uda_digit.py

--- outtest_cluster/uda_digit.py
+++ outtest_cluster/uda_digit.py
@@ -666,7 +666,7 @@ def obtain_label(loader, netF, netB, netC, args, c=None):
     # print("a")
     # print(all_fea.shape)
     perm = torch.randperm(60000)
-    idx = perm[:500]
+    idx = perm[:200]
     all_fea_1 = all_fea[idx]
     distance = cdist(all_fea, all_fea_1, p=2)
     # print(distance.shape)
