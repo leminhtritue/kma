281dfb94b1a242fc09fcf4965d7f6803953bc40c
leminhtritue <leminhtritue@gmail.com>
2021-01-02 19:18:23 +0700

    Update uda_digit.py

--- our_maxmax_4096/uda_digit.py
+++ our_maxmax_4096/uda_digit.py
@@ -352,7 +352,9 @@ def cal_acc_knn(loader, netF, netB, netC, ouput_name, label_name):
     print(collections.Counter(all_output_10_420_clone.cpu().numpy()))
 
     print("Stat")
-    print(collections.Counter(all_label_420.cpu().numpy()).items())
+    t = collections.Counter(all_label_420.cpu().numpy())
+    sorted(t.items(), key=itemgetter(0))
+    print(t)
     tt = 0
     for i in range(10):
     	t = all_label_420[all_label_420 == i]
