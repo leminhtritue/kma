09ff2677aad8d0df0b0816471baf27923d7dcedf
leminhtritue <leminhtritue@gmail.com>
2021-01-02 19:19:08 +0700

    Update uda_digit.py

--- our_maxmax_4096/uda_digit.py
+++ our_maxmax_4096/uda_digit.py
@@ -15,6 +15,7 @@ from scipy.spatial.distance import cdist
 import pickle
 from data_load import mnist, svhn, usps
 import collections
+from operator import itemgetter
 
 def op_copy(optimizer):
     for param_group in optimizer.param_groups:
