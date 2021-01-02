a672fdc6795ca7addb47246e49aeec504cf946d8
leminhtritue <leminhtritue@gmail.com>
2021-01-02 17:50:23 +0700

    Update uda_digit.py

--- our_maxmax_4096/uda_digit.py
+++ our_maxmax_4096/uda_digit.py
@@ -353,7 +353,8 @@ def cal_acc_knn(loader, netF, netB, netC, ouput_name, label_name):
     print(dist_420_4096.shape)
     idx = torch.topk(dist_420_4096, 100, dim=1,largest=False).indices
     print(idx.shape)
-    # predict
+    predict_100 = predict[idx]
+    print(predict_100.shape)
 
     sys.exit()
 
