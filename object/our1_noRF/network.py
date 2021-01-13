b040548772ae52456231745ba8c9723def64fe1d
leminhtritue <leminhtritue@gmail.com>
2021-01-14 04:37:27 +0700

    noRF

---
+++ object/our1_noRF/network.py
@@ -0,0 +1,231 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import models
+from torch.autograd import Variable
+import math
+import torch.nn.utils.weight_norm as weightNorm
+from collections import OrderedDict
+
+def calc_coeff(iter_num, high=1.0, low=0.0, alpha=10.0, max_iter=10000.0):
+    return np.float(2.0 * (high - low) / (1.0 + np.exp(-alpha*iter_num / max_iter)) - (high - low) + low)
+
+def init_weights(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1 or classname.find('ConvTranspose2d') != -1:
+        nn.init.kaiming_uniform_(m.weight)
+        nn.init.zeros_(m.bias)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight, 1.0, 0.02)
+        nn.init.zeros_(m.bias)
+    elif classname.find('Linear') != -1:
+        nn.init.xavier_normal_(m.weight)
+        nn.init.zeros_(m.bias)
+
+vgg_dict = {"vgg11":models.vgg11, "vgg13":models.vgg13, "vgg16":models.vgg16, "vgg19":models.vgg19, 
+"vgg11bn":models.vgg11_bn, "vgg13bn":models.vgg13_bn, "vgg16bn":models.vgg16_bn, "vgg19bn":models.vgg19_bn} 
+class VGGBase(nn.Module):
+  def __init__(self, vgg_name):
+    super(VGGBase, self).__init__()
+    model_vgg = vgg_dict[vgg_name](pretrained=True)
+    self.features = model_vgg.features
+    self.classifier = nn.Sequential()
+    for i in range(6):
+        self.classifier.add_module("classifier"+str(i), model_vgg.classifier[i])
+    self.in_features = model_vgg.classifier[6].in_features
+
+  def forward(self, x):
+    x = self.features(x)
+    x = x.view(x.size(0), -1)
+    x = self.classifier(x)
+    return x
+
+res_dict = {"resnet18":models.resnet18, "resnet34":models.resnet34, "resnet50":models.resnet50, 
+"resnet101":models.resnet101, "resnet152":models.resnet152, "resnext50":models.resnext50_32x4d, "resnext101":models.resnext101_32x8d}
+
+class ResBase(nn.Module):
+    def __init__(self, res_name):
+        super(ResBase, self).__init__()
+        model_resnet = res_dict[res_name](pretrained=True)
+        self.conv1 = model_resnet.conv1
+        self.bn1 = model_resnet.bn1
+        self.relu = model_resnet.relu
+        self.maxpool = model_resnet.maxpool
+        self.layer1 = model_resnet.layer1
+        self.layer2 = model_resnet.layer2
+        self.layer3 = model_resnet.layer3
+        self.layer4 = model_resnet.layer4
+        self.avgpool = model_resnet.avgpool
+        self.in_features = model_resnet.fc.in_features
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+class FeatureMap(nn.Module):
+    """Define the FeatureMap interface."""
+    def __init__(self, query_dims):
+        super().__init__()
+        self.query_dims = query_dims
+
+    def new_feature_map(self):
+        """Create a new instance of this feature map. In particular, if it is a
+        random feature map sample new parameters."""
+        raise NotImplementedError()
+
+    def forward_queries(self, x):
+        """Encode the queries `x` using this feature map."""
+        return self(x)
+
+    def forward_keys(self, x):
+        """Encode the keys `x` using this feature map."""
+        return self(x)
+
+    def forward(self, x):
+        """Encode x using this feature map. For symmetric feature maps it
+        suffices to define this function, but for asymmetric feature maps one
+        needs to define the `forward_queries` and `forward_keys` functions."""
+        raise NotImplementedError()
+
+    @classmethod
+    def factory(cls, *args, **kwargs):
+        """Return a function that when called with the query dimensions returns
+        an instance of this feature map.
+
+        It is inherited by the subclasses so it is available in all feature
+        maps.
+        """
+        def inner(query_dims):
+            return cls(query_dims, *args, **kwargs)
+        return inner
+
+class RandomFourierFeatures(FeatureMap):
+    def __init__(self, query_dimensions, n_dims=None, gamma = 0.5, softmax_temp=None,
+                 orthogonal=False):
+        super(RandomFourierFeatures, self).__init__(query_dimensions)
+
+        self.n_dims = n_dims or query_dimensions
+        self.orthogonal = orthogonal
+        self.softmax_temp = (
+            1/math.sqrt(query_dimensions) if softmax_temp is None
+            else softmax_temp
+        )
+        self.gamma = gamma
+
+        # Make a buffer for storing the sampled omega
+        self.register_buffer(
+            "omega",
+            torch.zeros(query_dimensions, self.n_dims//2)
+        )
+
+    def new_feature_map(self):
+        if self.orthogonal:
+            orthogonal_random_matrix_(self.omega)
+        else:
+            self.omega.normal_()
+
+    def forward(self, x):
+        # print(x.shape)
+        # x = x * math.sqrt(self.softmax_temp)
+        x = x * self.gamma
+        u = x.matmul(self.omega)
+        phi = torch.cat([torch.cos(u), torch.sin(u)], dim=-1)
+        # return phi * math.sqrt(2/self.n_dims)
+        return phi
+
+class feat_bootleneck(nn.Module):
+    def __init__(self, feature_dim, gamma = 0.5, feature_flag =False, bottleneck_dim=256, type="ori"):
+        super(feat_bootleneck, self).__init__()
+        self.bn = nn.BatchNorm1d(bottleneck_dim, affine=True)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(p=0.5)
+        self.bottleneck = nn.Linear(feature_dim, bottleneck_dim)
+        self.bottleneck.apply(init_weights)
+        self.type = type
+
+        self.feature_map = RandomFourierFeatures(bottleneck_dim, 512, gamma)
+        self.feature_map.new_feature_map()
+        self.feature_flag = feature_flag
+
+    def forward(self, x):
+        x = self.bottleneck(x)
+        if self.type == "bn":
+            x = self.bn(x)
+
+        # x = self.feature_map(x)
+        return x
+
+class feat_classifier(nn.Module):
+    def __init__(self, class_num, bottleneck_dim=256, type="linear"):
+        super(feat_classifier, self).__init__()
+        self.type = type
+        if type == 'wn':
+            self.fc = weightNorm(nn.Linear(bottleneck_dim, class_num), name="weight")
+            # self.fc = weightNorm(nn.Linear(512, class_num), name="weight")
+            self.fc.apply(init_weights)
+        else:
+            self.fc = nn.Linear(bottleneck_dim, class_num)
+            # self.fc = nn.Linear(512, class_num)
+            self.fc.apply(init_weights)
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+    def get_weight(self):
+        return self.fc.weight
+    def get_bias(self):
+        return self.fc.bias
+
+class feat_classifier_two(nn.Module):
+    def __init__(self, class_num, input_dim, bottleneck_dim=256):
+        super(feat_classifier_two, self).__init__()
+        self.type = type
+        self.fc0 = nn.Linear(input_dim, bottleneck_dim)
+        self.fc0.apply(init_weights)
+        self.fc1 = nn.Linear(bottleneck_dim, class_num)
+        self.fc1.apply(init_weights)
+
+    def forward(self, x):
+        x = self.fc0(x)
+        x = self.fc1(x)
+        return x
+
+class Res50(nn.Module):
+    def __init__(self):
+        super(Res50, self).__init__()
+        model_resnet = models.resnet50(pretrained=True)
+        self.conv1 = model_resnet.conv1
+        self.bn1 = model_resnet.bn1
+        self.relu = model_resnet.relu
+        self.maxpool = model_resnet.maxpool
+        self.layer1 = model_resnet.layer1
+        self.layer2 = model_resnet.layer2
+        self.layer3 = model_resnet.layer3
+        self.layer4 = model_resnet.layer4
+        self.avgpool = model_resnet.avgpool
+        self.in_features = model_resnet.fc.in_features
+        self.fc = model_resnet.fc
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        y = self.fc(x)
+        return x, y
