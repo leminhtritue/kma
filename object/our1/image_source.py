e6e46b75f464b66f0d460896ce431dfc14585d6c
leminhtritue <leminhtritue@gmail.com>
2021-01-14 07:04:05 +0700

    Update image_source.py

--- object/our1/image_source.py
+++ object/our1/image_source.py
@@ -230,9 +230,9 @@ def train_source(args):
     interval_iter = max_iter // 10
     iter_num = 0
 
-    # netF.train()
-    # netB.train()
-    # netC.train()
+    netF.train()
+    netB.train()
+    netC.train()
 
     total_loss = 0.0
     count_loss = 0
@@ -262,9 +262,9 @@ def train_source(args):
         optimizer.step()
 
         if iter_num % interval_iter == 0 or iter_num == max_iter:
-            # netF.eval()
-            # netB.eval()
-            # netC.eval()
+            netF.eval()
+            netB.eval()
+            netC.eval()
             if args.dset=='VISDA-C':
                 acc_s_te, acc_list = cal_acc(dset_loaders['source_te'], netF, netB, netC, True)
                 log_str = 'Task: {}, Iter:{}/{}; Accuracy = {:.2f}%'.format(args.name_src, iter_num, max_iter, acc_s_te) + '\n' + acc_list
