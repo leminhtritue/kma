3443d16c658aad49324a4dc21c635d3a8275a86b
leminhtritue <leminhtritue@gmail.com>
2021-01-22 18:55:53 +0700

    cmp WH

--- object/ourshot/cmd_signle.bat
+++ object/ourshot/cmd_signle.bat
@@ -38,3 +38,14 @@ python image_target.py --nrf 16384 --s 1 --t 0 --output_src ckps/source1_16384_g
 python image_target.py --nrf 16384 --s 1 --t 0 --output_src ckps/source1_16384_gm0_1_aw0_1_vat0_arf1_ep500/ --w_vat 1.0 --cls_par 0.1 --alpha_rf 0.1 --max_zero 0.0 --gpu_id 1
 python image_target.py --nrf 16384 --s 1 --t 2 --output_src ckps/source1_16384_gm0_1_aw0_1_vat0_arf1_ep500/ --w_vat 1.0 --cls_par 0.0 --alpha_rf 0.1 --max_zero 0.0 --gpu_id 2
 python image_target.py --nrf 16384 --s 1 --t 2 --output_src ckps/source1_16384_gm0_1_aw0_1_vat0_arf1_ep500/ --w_vat 1.0 --cls_par 0.1 --alpha_rf 0.1 --max_zero 0.0 --gpu_id 3
+
+
+#Grid image_target
+python image_target.py --nrf 16384 --s 2 --t 0 --output_src ckps/source2_16384_gm0_1_aw0_1_vat0_arf0_1_ep30/ --grid 1.0 --gpu_id 0
+python image_target.py --nrf 16384 --s 2 --t 0 --output_src ckps/source2_16384_gm0_1_aw0_1_vat0_arf1_ep30/ --grid 1.0 --gpu_id 1
+python image_target.py --nrf 16384 --s 2 --t 0 --output_src ckps/source2_16384_gm0_1_aw0_1_vat1_arf0_1_ep30/ --grid 1.0 --gpu_id 2
+python image_target.py --nrf 16384 --s 2 --t 0 --output_src ckps/source2_16384_gm0_1_aw0_1_vat1_arf1_ep30/ --grid 1.0 --gpu_id 3
+python image_target.py --nrf 16384 --s 2 --t 0 --output_src ckps/source2_16384_gm0_1_aw0_1_vat0_arf0_1_e500/ --grid 1.0 --gpu_id 0
+python image_target.py --nrf 16384 --s 2 --t 0 --output_src ckps/source2_16384_gm0_1_aw0_1_vat0_arf1_ep500/ --grid 1.0 --gpu_id 1
+python image_target.py --nrf 16384 --s 2 --t 0 --output_src ckps/source2_16384_gm0_1_aw0_1_vat1_arf0_1_ep500/ --grid 1.0 --gpu_id 2
+python image_target.py --nrf 16384 --s 2 --t 0 --output_src ckps/source2_16384_gm0_1_aw0_1_vat1_arf1_ep500/ --grid 1.0 --gpu_id 3
