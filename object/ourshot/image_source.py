0c3ee3cdea68221848f25fd17b92f210ba0d9408
leminhtritue <leminhtritue@gmail.com>
2021-02-24 22:58:25 +0700

    Update image_source.py

--- object/ourshot/image_source.py
+++ object/ourshot/image_source.py
@@ -452,9 +452,9 @@ if __name__ == "__main__":
     parser.add_argument('--w_vat', type=float, default=0.0)
     parser.add_argument('--radius', type=float, default=0.01)
 
-    parser.add_argument('--debug_in000', type=float, default=0.1)
-    parser.add_argument('--debug_in001', type=float, default=0.1)
-    parser.add_argument('--debug_in002', type=float, default=0.1)
+    parser.add_argument('--debug_in000', type=float, default=0.0)
+    parser.add_argument('--debug_in001', type=float, default=0.0)
+    parser.add_argument('--debug_in002', type=float, default=0.0)
     parser.add_argument('--debug_turnon001', type=float, default=0.1)
     parser.add_argument('--debug_turnon002', type=float, default=0.1)
 
