6aad7836eb7d489c0970523582170c9822bafa16
leminhtritue <leminhtritue@gmail.com>
2021-03-03 19:29:55 +0700

    testtarget

--- object/ourshot/image_target.py
+++ object/ourshot/image_target.py
@@ -521,6 +521,362 @@ def test_target(args):
     args.out_file.flush()
     print(log_str)
 
+def train_target2(args):
+    dset_loaders = data_load(args)
+    ## set base network
+    if args.net[0:3] == 'res':
+        netF = network.ResBase(res_name=args.net).cuda()
+    elif args.net[0:3] == 'vgg':
+        netF = network.VGGBase(vgg_name=args.net).cuda()  
+
+    netB = network.feat_bootleneck(type=args.classifier, feature_dim=netF.in_features, bottleneck_dim=args.bottleneck).cuda()
+    netC = network.feat_classifier(type=args.layer, class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
+
+    netBRF = network.feat_bootleneck_rf(nrf=args.nrf, type=args.classifier, gamma = args.gamma, bottleneck_dim=args.bottleneck).cuda()
+    netCRF = network.feat_classifier_rf(nrf=args.nrf, type=args.layer_rf, class_num = args.class_num).cuda()
+    
+    modelpath = args.output_dir + '/source_F.pt'   
+    netF.load_state_dict(torch.load(modelpath))
+    modelpath = args.output_dir + '/source_B.pt'   
+    netB.load_state_dict(torch.load(modelpath))
+
+    modelpath = args.output_dir + '/source_C.pt'    
+    netC.load_state_dict(torch.load(modelpath))
+
+    if args.fromShot == 0.0:
+        modelpath = args.output_dir_src + '/source_BRF.pt'    
+        netBRF.load_state_dict(torch.load(modelpath))
+        modelpath = args.output_dir_src + '/source_CRF.pt'    
+        netCRF.load_state_dict(torch.load(modelpath))
+
+    param_group = []
+    if args.train_c == 0.0:
+        netC.eval()
+        for k, v in netC.named_parameters():
+            v.requires_grad = False
+    else:
+        for k, v in netC.named_parameters():
+            if args.lr_decayc > 0:
+                param_group += [{'params': v, 'lr': args.lr * args.lr_decayc}]
+            else:
+                v.requires_grad = False        
+
+    if args.train_rf == 0.0:
+        netCRF.eval()
+        for k, v in netCRF.named_parameters():
+            v.requires_grad = False
+    else:
+        for k, v in netCRF.named_parameters():
+            if args.lr_decay2 > 0:
+                param_group += [{'params': v, 'lr': args.lr * args.lr_decay2}]
+            else:
+                v.requires_grad = False   
+
+
+    
+    for k, v in netF.named_parameters():
+        if args.lr_decay1 > 0:
+            param_group += [{'params': v, 'lr': args.lr * args.lr_decay1}]
+        else:
+            v.requires_grad = False
+    for k, v in netB.named_parameters():
+        if args.lr_decay2 > 0:
+            param_group += [{'params': v, 'lr': args.lr * args.lr_decay2}]
+        else:
+            v.requires_grad = False
+
+    for k, v in netBRF.named_parameters():
+        if args.lr_decay2 > 0:
+            param_group += [{'params': v, 'lr': args.lr * args.lr_decay2}]
+        else:
+            v.requires_grad = False
+
+    optimizer = optim.SGD(param_group)
+    optimizer = op_copy(optimizer)
+
+    max_iter = args.max_epoch * len(dset_loaders["target"])
+    # interval_iter = max_iter // args.interval
+    interval_iter = len(dset_loaders["target"])
+    iter_num = 0
+
+    classifier_loss_total = 0.0
+    classifier_loss_count = 0
+    # entropy_loss_total = 0.0
+    # entropy_loss_count = 0
+    # costlog_loss_total = 0.0
+    # costlog_loss_count = 0
+    # costs_loss_total = 0.0
+    # costs_loss_count = 0
+    # costdist_loss_total = 0.0
+    # costdist_loss_count = 0
+    # right_sample_count = 0
+    # sum_sample = 0
+    # start_output = True
+
+    while iter_num < max_iter:
+        try:
+            inputs_test, _, tar_idx = iter_test.next()
+        except:
+            iter_test = iter(dset_loaders["target"])
+            inputs_test, _, tar_idx = iter_test.next()
+
+        if inputs_test.size(0) == 1:
+            continue
+
+        if iter_num % interval_iter == 0 and args.cls_par > 0:
+            netF.eval()
+            netB.eval()
+            netBRF.eval()
+            if args.train_c != 0.0:
+                netC.eval()
+            if args.train_rf != 0.0:
+                netCRF.eval()
+            mem_label = obtain_label(dset_loaders['test'], netF, netB, netC, args)
+            mem_label = torch.from_numpy(mem_label).cuda()
+            netF.train()
+            netB.train()
+            netBRF.train()
+            if args.train_c != 0.0:
+                netC.train()
+            if args.train_rf != 0.0:
+                netCRF.train()
+
+        if iter_num % interval_iter == 0 and args.cls_parrf > 0:
+            netF.eval()
+            netB.eval()
+            netBRF.eval()
+            if args.train_c != 0.0:
+                netC.eval()
+            if args.train_rf != 0.0:
+                netCRF.eval()
+            mem_label_rf = obtain_labelrf(dset_loaders['test'], netF, netB, netBRF, netCRF, args)
+            mem_label_rf = torch.from_numpy(mem_label_rf).cuda()
+            netF.train()
+            netB.train()
+            netBRF.train()
+            if args.train_c != 0.0:
+                netC.train()
+            if args.train_rf != 0.0:
+                netCRF.train()
+
+        inputs_test = inputs_test.cuda()
+
+        iter_num += 1
+        lr_scheduler(optimizer, iter_num=iter_num, max_iter=max_iter)
+
+        features_test = netB(netF(inputs_test))
+        outputs_test = netC(features_test)
+        outputs_test_rf = netCRF(netBRF(features_test.detach()))
+
+        if args.cls_par > 0:
+            pred = mem_label[tar_idx]
+            classifier_loss = nn.CrossEntropyLoss()(outputs_test, pred)
+            classifier_loss *= args.cls_par
+            if iter_num < interval_iter and args.dset == "VISDA-C":
+                classifier_loss *= 0
+        else:
+            classifier_loss = torch.tensor(0.0).cuda()
+
+        if args.cls_parrf > 0:
+            pred_rf = mem_label_rf[tar_idx]
+            classifier_loss_rf = args.cls_parrf * nn.CrossEntropyLoss()(outputs_test_rf, pred_rf)
+            if iter_num < interval_iter and args.dset == "VISDA-C":
+                classifier_loss_rf *= 0
+            classifier_loss += classifier_loss_rf
+
+
+        softmax_out = nn.Softmax(dim=1)(outputs_test)
+        if args.ent:
+            entropy_loss = torch.mean(loss.Entropy(softmax_out))
+            if args.gent:
+                msoftmax = softmax_out.mean(dim=0)
+                gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + args.epsilon))
+                entropy_loss -= gentropy_loss
+            im_loss = entropy_loss * args.ent_par
+            classifier_loss += im_loss
+
+
+
+        mark_max = torch.zeros(outputs_test_rf.size()).cuda()
+        
+        mark_zeros = torch.zeros(outputs_test_rf.size()).cuda()
+        if (args.max_zero > 0.0):
+            outputs_test_max = torch.maximum(outputs_test_rf, mark_zeros)
+        else:
+            outputs_test_max = outputs_test_rf
+
+        for i in range(args.class_num):
+            mark_max[:,i] = torch.max(torch.cat((outputs_test_max[:, :i],outputs_test_max[:, i+1:]), dim = 1), dim = 1).values        
+        cost_s = outputs_test_max - mark_max
+   
+        softmax_si = nn.Softmax(dim=1)(cost_s)
+
+        if args.alpha_rf > 0:
+
+            entropy_si = -(softmax_out * torch.log(softmax_si + 1e-5))
+            entropy_si = torch.sum(entropy_si, dim=1)
+            entropy_si_loss = torch.mean(entropy_si)
+            classifier_loss += args.alpha_rf * entropy_si_loss
+
+        if args.alpha_rfen > 0:
+            entropy_loss_rf = torch.mean(loss.Entropy(softmax_si))
+
+            msoftmax_rf = softmax_si.mean(dim=0)
+            gentropy_loss_rf = torch.sum(-msoftmax_rf * torch.log(msoftmax_rf + args.epsilon))
+            entropy_loss_rf -= gentropy_loss_rf
+            im_loss_rf = entropy_loss_rf * args.alpha_rfen
+            classifier_loss += im_loss_rf
+
+        if (args.w_vat > 0):
+        	eps = (torch.randn(size=inputs_test.size())).type(inputs_test.type())
+        	eps = 1e-6 * normalize_perturbation(eps)
+        	eps.requires_grad = True
+        	outputs_source_adv_eps = netC(netB(netF(inputs_test + eps)))
+        	loss_func_nll = KLDivWithLogits()
+        	loss_eps  = loss_func_nll(outputs_source_adv_eps, outputs_test.detach())
+        	loss_eps.backward()
+        	eps_adv = eps.grad
+        	eps_adv = normalize_perturbation(eps_adv)
+        	inputs_source_adv = inputs_test + args.radius * eps_adv
+        	output_source_adv = netC(netB(netF(inputs_source_adv.detach())))
+        	loss_vat     = loss_func_nll(output_source_adv, outputs_test.detach())
+        	classifier_loss += args.w_vat * loss_vat
+
+        classifier_loss_total += classifier_loss
+        classifier_loss_count += 1   
+        # entropy_loss_total += entropy_loss
+        # entropy_loss_count += 1 
+        # costlog_loss_total += 0
+        # costlog_loss_count += 1  
+        # costs_loss_total += 0
+        # costs_loss_count += 1  
+        # costdist_loss_total += 0
+        # costdist_loss_count += 1 
+
+        # max_hyperplane = outputs_test.max(dim=1).values       
+        # max_hyperplane[max_hyperplane > 0] = 1
+        # max_hyperplane[max_hyperplane < 0] = 0
+        # right_sample_count += max_hyperplane.sum()
+        # sum_sample += outputs_test.shape[0]
+
+        # if (start_output):
+        #     all_output = outputs_test.float().cpu()
+        #     start_output = False
+        # else:
+        #     all_output = torch.cat((all_output, outputs_test.float().cpu()), 0)
+
+        optimizer.zero_grad()
+        classifier_loss.backward()
+        optimizer.step()
+
+        if iter_num % interval_iter == 0 or iter_num == max_iter:
+            netF.eval()
+            netB.eval()
+            netBRF.eval()
+            if args.train_c != 0.0:
+                netC.eval()
+            if args.train_rf != 0.0:
+                netCRF.eval()
+            if args.dset=='VISDA-C':
+                acc_s_te, acc_list = cal_acc(dset_loaders['test'], netF, netB, netC, True)
+                log_str = 'Task: {}, Iter:{}/{}; Accuracy = {:.2f}%'.format(args.name, iter_num, max_iter, acc_s_te) + '\n' + acc_list
+            else:
+                # _, predict = torch.max(all_output, 1)
+                acc_s_te, _ = cal_acc(dset_loaders['test'], netF, netB, netC, False)
+                acc_s_tr, _ = cal_acc(dset_loaders['target'], netF, netB, netC, False)
+                acc_s_te_rf, _ = cal_accrf(dset_loaders['test'], netF, netB, netBRF, netCRF, False)
+                acc_s_tr_rf, _ = cal_accrf(dset_loaders['target'], netF, netB, netBRF, netCRF, False)
+
+                # log_str = 'Task: {}, Iter:{}/{}; Loss : {:.2f}, , Accuracy target (train/test) = {:.2f}% / {:.2f}%, moved samples: {}/{}.'.format(args.name, iter_num, max_iter, \
+                # classifier_loss_total/classifier_loss_count, acc_s_tr, acc_s_te, right_sample_count, sum_sample)
+                log_str = 'Task: {}, Iter:{}/{}; Loss : {:.2f}, , Accuracy target (train/test/trainrf/testrf) = {:.2f}% / {:.2f}% / {:.2f}% / {:.2f}%.'.format(args.name, iter_num, max_iter, \
+                classifier_loss_total/classifier_loss_count, acc_s_tr, acc_s_te, acc_s_tr_rf, acc_s_te_rf)
+
+            args.out_file.write(log_str + '\n')
+            args.out_file.flush()
+            print(log_str+'\n')
+
+            if acc_s_te < 50:
+                return netF, netB, netC, (acc_s_te + 100)
+
+            classifier_loss_total = 0.0
+            classifier_loss_count = 0
+            # entropy_loss_total = 0.0
+            # entropy_loss_count = 0
+            # costs_loss_total = 0.0
+            # costs_loss_count = 0
+            # costdist_loss_total = 0.0
+            # costdist_loss_count = 0
+            # costlog_loss_total = 0.0
+            # costlog_loss_count = 0
+            # right_sample_count = 0
+            # sum_sample = 0
+            # start_output = True
+
+            netF.train()
+            netB.train()
+            netBRF.train()
+            if args.train_c != 0.0:
+                netC.train()
+            if args.train_rf != 0.0:
+                netCRF.train()
+
+    if args.issave:   
+        torch.save(netF.state_dict(), osp.join(args.output_dir, "target_F_" + args.savename + ".pt"))
+        torch.save(netB.state_dict(), osp.join(args.output_dir, "target_B_" + args.savename + ".pt"))
+        torch.save(netC.state_dict(), osp.join(args.output_dir, "target_C_" + args.savename + ".pt"))
+        torch.save(netBRF.state_dict(), osp.join(args.output_dir, "target_BRF_" + args.savename + ".pt"))
+        torch.save(netCRF.state_dict(), osp.join(args.output_dir, "target_CRF_" + args.savename + ".pt"))
+        
+    return netF, netB, netC, acc_s_te
+
+def test_target2_1(args):
+    dset_loaders = data_load(args)
+    ## set base network
+    if args.net[0:3] == 'res':
+        netF = network.ResBase(res_name=args.net).cuda()
+    elif args.net[0:3] == 'vgg':
+        netF = network.VGGBase(vgg_name=args.net).cuda()  
+
+    netB = network.feat_bootleneck(type=args.classifier, feature_dim=netF.in_features, bottleneck_dim=args.bottleneck).cuda()
+    netC = network.feat_classifier(type=args.layer, class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
+
+    netB = network.feat_bootleneck(type=args.classifier, feature_dim=netF.in_features, bottleneck_dim=args.bottleneck).cuda()
+    netC = network.feat_classifier(type=args.layer, class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
+
+    args.modelpath = args.output_dir + '/target_F_' + args.savename + ".pt" 
+    netF.load_state_dict(torch.load(args.modelpath))
+    args.modelpath = args.output_dir + '/target_B_' + args.savename + ".pt" 
+    netB.load_state_dict(torch.load(args.modelpath))
+    args.modelpath = args.output_dir + '/target_C_' + args.savename + ".pt" 
+    netC.load_state_dict(torch.load(args.modelpath))
+    args.modelpath = args.output_dir + '/target_BRF_' + args.savename + ".pt"     
+    netBRF.load_state_dict(torch.load(modelpath))
+    args.modelpath = args.output_dir + '/target_CRF_' + args.savename + ".pt"     
+    netCRF.load_state_dict(torch.load(modelpath))
+
+    netF.eval()
+    netB.eval()
+    netC.eval()
+    netBRF.eval()
+    netCRF.eval()
+
+    if args.da == 'oda':
+        acc_os1, acc_os2, acc_unknown = cal_acc_oda(dset_loaders['test'], netF, netB, netC)
+        log_str = '\nTraining: {}, Task: {}, Accuracy = {:.2f}% / {:.2f}% / {:.2f}%'.format(args.trte, args.name, acc_os2, acc_os1, acc_unknown)
+    else:
+        if args.dset=='VISDA-C':
+            acc, acc_list = cal_acc(dset_loaders['test'], netF, netB, netC, True)
+            log_str = '\nTraining: {}, Task: {}, Accuracy = {:.2f}%'.format(args.trte, args.name, acc) + '\n' + acc_list
+        else:
+            acc_h, _ = cal_acc(dset_loaders['test'], netF, netB, netC, False)
+            acc_rf, _ = cal_accrf(dset_loaders['test'], netF, netB, netBRF, netCRF, False)
+            log_str = '\nTraining:, Task: {}, Accuracy (h/rf) = {:.2f}% / {:.2f}%'.format(args.name, acc_h, acc_rf)
+
+    args.out_file.write(log_str)
+    args.out_file.flush()
+    print(log_str)
+
 def print_args(args):
     s = "==========================================\n"
     for arg, content in args.__dict__.items():
@@ -764,7 +1120,9 @@ if __name__ == "__main__":
         args.out_file = open(osp.join(args.output_dir, 'log_' + args.savename + '.txt'), 'w')
         args.out_file.write(print_args(args)+'\n')
         args.out_file.flush()
-        test_target(args)
+        # test_target(args)
+        test_target2_1(args)
+        sys.exit()
 
         if (args.grid > 0.0):
             list_cls_par = [0.3, 0.1, 1.0]
