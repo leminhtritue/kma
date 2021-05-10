28ed1eaf63da4dc87bb5bfbc44592a43690bb227
leminhtritue <leminhtritue@gmail.com>
2021-05-10 11:16:46 +0700

    Update OOD_Baseline_and_ODIN.py

--- object/ood/OOD_Baseline_and_ODIN.py
+++ object/ood/OOD_Baseline_and_ODIN.py
@@ -31,7 +31,7 @@ def main():
     pre_trained_net = './pre_trained1/' + args.net_type + '_' + args.dataset + '.pth'
     args.outf = args.outf + args.net_type + '_' + args.dataset + '/'
     if os.path.isdir(args.outf) == False:
-        os.mkdir(args.outf)
+        os.makedirs(args.outf)
     torch.cuda.manual_seed(0)
     torch.cuda.set_device(args.gpu)
     
