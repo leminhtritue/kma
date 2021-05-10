7b6ab6e26064a80ce567002723041602b27d8069
leminhtritue <leminhtritue@gmail.com>
2021-05-10 15:55:33 +0700

    ood

--- object/ood/OOD_Baseline_and_ODIN.py
+++ object/ood/OOD_Baseline_and_ODIN.py
@@ -73,7 +73,9 @@ def main():
 
         model = models.ResNet34(num_c=args.num_classes)
         model.load_state_dict(torch.load(pre_trained_net, map_location = "cuda:" + str(args.gpu)))
-        in_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+
+        in_transform = transforms.Compose([transforms.Resize(32),transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        # in_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
     model.cuda()
     print('load model: ' + args.net_type)
     
